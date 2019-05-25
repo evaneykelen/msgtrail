53e7c972f381bbf195108ef4a4f06251be9864c2
Erik van Eykelen <erik.van.eykelen@bitgain.com>
2019-05-25 13:57:39 +0200

    Add titlecase method
    Based on John Gruber's title casing rules: https://daringfireball.net/2008/08/title_case_update

---
+++ gem/lib/msgtrail/titlecase.rb
@@ -0,0 +1,82 @@
+module Msgtrail
+  class Titlecase
+
+    SMALL_WORDS = [
+      /\Aa(nd|n|s|t)?\z/,
+      /\Ab(ut|y)\z/,
+      /\Aen\z/,
+      /\Afor\z/,
+      /\Ai(f|n)\z/,
+      /\Ao(f|n|r)\z/,
+      /\At(he|o)\z/,
+      /\Avs?\.?\z/
+    ].freeze
+
+    RE = Regexp.new('\b(a(nd|n|s|t)?|b(ut|y)|en|for|i(f|n)|o(f|n|r)|t(he|o)|vs?\.?)\b').freeze
+
+    def self.is_small_word?(word)
+      !RE.match(word).nil?
+    end
+
+    # "__foo" => "__Foo"
+    def self.upcase_first_real_letter(word)
+      word.sub(/[a-zA-Z0-9]/, &:upcase)
+    end
+
+    # step-by-step => Step-by-Step
+    def self.upcase_word_with_dashes(word)
+      word.split('-').map { |part| is_small_word?(part) ? part : part.capitalize }.join("-")
+    end
+
+    # before/after => Before/After
+    def self.upcase_word_with_slashes(word)
+      word.split('/').map { |part| part.capitalize }.join('/')
+    end
+
+    def self.titlecase(str)
+      return if (str || '').strip.empty?
+
+      # Downcase an all-upcase sentence
+      str.downcase! if str.scan(/[A-Z]|\s|\W/).length == str.length
+
+      # Replace tabs by single space
+      # Replace weird spaces by regular space
+      # Split sentence at space boundaries
+      word_arr = str.gsub(/\t/, ' ').gsub("\u{2011}", ' ').split(' ')
+
+      # Initialize operand array
+      operand_arr = Array.new(word_arr.size, :nop)
+
+      word_arr.each_with_index do |word, idx|
+        # Don't capitalize small words...
+        # ... unless it's first word
+        # ... unless it's last word
+        # ... unless word is preceded by word ending with colon
+        operand_arr[idx] = :do_not_upcase  if idx != 0 && idx != word_arr.size - 1 && is_small_word?(word) && word_arr[idx - 1][-1] != ':'
+        # Don't simply capitalize first letter if word starts with (, _, ', or "
+        operand_arr[idx] = :upcase_later   if word[0].match(/\(|_|'|"/)
+        # Capitalize first letter and letters preceded by -
+        operand_arr[idx] = :upcase_dashed  if word.count('-') > 0
+        # Capitalize letters preceded by / inside word
+        operand_arr[idx] = :upcase_slashed if word[1..].count('/') > 0
+        # Don't capitalize word if it starts with /
+        operand_arr[idx] = :do_not_upcase  if word[0] == '/'
+        # Don't capitalize URLs
+        operand_arr[idx] = :do_not_upcase  if word.match(/https?:\/\//i)
+        # Don't capitalize words containing capitals besides first letter
+        # Don't capitalize words containing dots inside word
+        operand_arr[idx] = :do_not_upcase  if word[1..].match(/[A-Z]/) || word[1..-3].match(/\.|&/)
+      end
+
+      word_arr.each_with_index do |word, idx|
+        word_arr[idx] = upcase_first_real_letter(word) if operand_arr[idx] == :upcase_later
+        word_arr[idx] = upcase_word_with_dashes(word)  if operand_arr[idx] == :upcase_dashed
+        word_arr[idx] = upcase_word_with_slashes(word) if operand_arr[idx] == :upcase_slashed
+        word_arr[idx] = word.capitalize                if operand_arr[idx] == :nop
+      end
+
+      return word_arr.join(' ')
+    end
+
+  end
+end
